c27edbd28c01d5e44c28baa90f4c599026982793
job-hunter-pad <iacobradu33@yahoo.com>
2021-04-05 23:25:23 +0300

    Initial commit

---
+++ src/test/java/jobhunter/employerservice/EmployerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package jobhunter.employerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
