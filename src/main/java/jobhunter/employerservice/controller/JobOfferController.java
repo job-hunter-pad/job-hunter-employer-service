6fe50531a02b7b4a7b08906947a86c9459fa8259
job-hunter-pad <iacobradu33@yahoo.com>
2021-04-16 08:40:25 +0300

    Added job application handling

--- src/main/java/jobhunter/employerservice/controller/JobOfferController.java
+++ src/main/java/jobhunter/employerservice/controller/JobOfferController.java
@@ -2,16 +2,20 @@ package jobhunter.employerservice.controller;
 
 import jobhunter.employerservice.controller.dto.CreateJobOfferDTO;
 import jobhunter.employerservice.controller.dto.UpdateJobOfferDTO;
+import jobhunter.employerservice.model.JobApplication;
+import jobhunter.employerservice.model.JobApplicationStatus;
 import jobhunter.employerservice.model.JobOffer;
 import jobhunter.employerservice.repository.JobOfferRepository;
 import jobhunter.employerservice.utils.StringValidation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.batch.BatchProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 public class JobOfferController {
@@ -76,4 +80,37 @@ public class JobOfferController {
         }
         throw new ResponseStatusException(HttpStatus.NOT_FOUND);
     }
+
+    @GetMapping("/getJobApplications/{jobId}")
+    public List<JobApplication> getJobApplications(@PathVariable String jobId) {
+        JobOffer jobOffer = jobOfferRepository.findById(jobId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        return jobOffer.getApplications();
+    }
+
+    @PostMapping("/acceptApplication/{jobId}/{applicationId}")
+    public void acceptApplication(@PathVariable String jobId, @PathVariable String applicationId) {
+        JobOffer jobOffer = jobOfferRepository.findById(jobId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        Optional<JobApplication> jobApplication = jobOffer.getApplications()
+                .stream()
+                .filter(application -> application.getId().equals(applicationId))
+                .findFirst();
+
+        if (jobApplication.isPresent()) {
+            jobApplication.get().setStatus(JobApplicationStatus.ACCEPTED);
+        }
+    }
+
+    @PostMapping("/rejectApplication/{jobId}/{applicationId}")
+    public void rejectApplication(@PathVariable String jobId, @PathVariable String applicationId) {
+        JobOffer jobOffer = jobOfferRepository.findById(jobId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        Optional<JobApplication> jobApplication = jobOffer.getApplications()
+                .stream()
+                .filter(application -> application.getId().equals(applicationId))
+                .findFirst();
+
+        if (jobApplication.isPresent()) {
+            jobApplication.get().setStatus(JobApplicationStatus.REJECTED);
+        }
+    }
+
 }
--- src/main/java/jobhunter/employerservice/model/JobApplicationStatus.java
+++ src/main/java/jobhunter/employerservice/model/JobApplicationStatus.java
@@ -1,6 +1,7 @@
 package jobhunter.employerservice.model;
 
 public enum JobApplicationStatus {
+    ACCEPTED,
     COMPLETED,
     PENDING,
     REJECTED
